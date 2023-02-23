b41ef02befd7703f7cddb4bc3e184768b5ab008d
cheonyeji <ssaltuck@gmail.com>
2023-02-24 07:15:47 +0900

    babel setting

--- 2023_digitech/day5/app.js
+++ 2023_digitech/day5/app.js
@@ -1,7 +1,8 @@
-const express = require("express");
+import express from "express";
+import path from "path";
+import fs from "fs";
+
 const app = express();
-const path = require("path");
-const fs = require("fs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
